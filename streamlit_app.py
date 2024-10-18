5329303960484796d3d228ebb3c4bb2f76917600
PaigeYang <33799289+PaigeYang@users.noreply.github.com>
2024-10-18 15:13:59 -0400

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,20 @@
 import streamlit as st
+from transformers import pipeline
+
+### Create a GPT2 generator pipeline
+generator = pipeline('text-generation', model='gpt2')
+
+prompt = st.text_input("What is your prompt today?")
+length= st.number_input(
+    "The expected length of the response", value=20, placeholder="Type a number..."
+)
+
+
+### Generate the answer to the question "Damascus is a"
+st.write(
+generator(prompt, max_length=length, truncation=True)
+)
+
 
 st.title("🎈 My new app")
 st.write(
