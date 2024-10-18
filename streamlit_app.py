a7f9038b908f6b8b9275c9b720c4388a106b181e
PaigeYang <33799289+PaigeYang@users.noreply.github.com>
2024-10-18 16:04:05 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,23 +4,24 @@ from transformers import pipeline
 ### Create a GPT2 generator pipeline
 generator = pipeline("text-generation", model="gpt2")
 
-prompt = st.text_input("🎈 What is your prompt today?")
+st.title("🎈 My IS883 Week6 Assignment")
+
+prompt = st.text_input("What is your prompt today?")
 length= st.number_input(
     "The expected length of the response", value=20, placeholder="Type a number..."
 )
-
 
 ### Generate the answer to the question
 st.header("🎈 High level of creativity response")
 
 
 st.write(
-generator(prompt, max_length=length, temperature=0.7, truncation=True)[0]["generated_text"]
+generator(prompt, max_length=length, temperature=50, truncation=True)[0]["generated_text"]
 )
 
 st.header("🎈 Predictable response")
 st.write(
-generator(prompt, max_length=length, truncation=True)[0]["generated_text"]
+generator(prompt, max_length=length, temperature=0.5, truncation=True)[0]["generated_text"]
 )
 
 st.title("🎈 My new app")
