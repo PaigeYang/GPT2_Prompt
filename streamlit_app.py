56bd9b98f2e1deb49a5e92e25d4153b41a3b8636
PaigeYang <33799289+PaigeYang@users.noreply.github.com>
2024-10-18 16:16:41 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,13 +12,13 @@ length= st.number_input(
 )
 
 ### Generate the answer to the question
+### Set temperature as 100 for high level of creativity response
 st.header("High level of creativity response")
-
-
 st.write(
 generator(prompt, max_length=length, temperature=100.0, truncation=True)[0]["generated_text"]
 )
 
+### Set temperature as 0.1 for predictable response
 st.header("Predictable response")
 st.write(
 generator(prompt, max_length=length, temperature=0.1, truncation=True)[0]["generated_text"]
